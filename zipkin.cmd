9b022aad7c592abaedee2ca80cb83958b12e5865
Jhon Carlos Solis Ochoa <jcsolis78@gmail.com>
2020-03-06 14:38:56 -0500

    Se cambia el password de conexion a la base de datos y se coloca el servidor de mensajeria de pruebas

--- zipkin.cmd
+++ zipkin.cmd
@@ -1,6 +1,6 @@
 @echo off
-set RABBIT_ADDRESSES=rabbitmq-server:5672
+set RABBIT_ADDRESSES=rabbitmq83-server:5672
 set STORAGE_TYPE=mysql
 set MYSQL_USER=zipkin
-set MYSQL_PASS=zipkin
+set MYSQL_PASS=Andrea_1204
 java -jar ./zipkin-server-2.20.0-exec.jar
