4bbacc284017a4ff39ef6b5d1ca6a5cef67a4b83
Andrea Solis <jcsolis78@hotmail.com>
2020-03-10 11:46:57 -0500

    El dato de la variable BASURA en el archivo zipkin.cmd es importante

--- zipkin.cmd
+++ zipkin.cmd
@@ -4,4 +4,4 @@ set STORAGE_TYPE=mysql
 set MYSQL_USER=zipkin
 set MYSQL_PASS=Andrea_1204
 java -jar ./zipkin-server-2.20.0-exec.jar
-set BASURA=Se agrega la variable a eliminar
+set BASURA=Esta es una variable requerida
